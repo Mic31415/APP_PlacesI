63bdde0c4102688e1dee8bf6be88d4531440afa4
Dhaval Panchani <developer@identiqinfotech.com>
2026-01-23 14:41:54 +0530

    remove google map api key

--- src/constants/Config.ts
+++ src/constants/Config.ts
@@ -1,5 +1,5 @@
 export const Config = {
-    GOOGLE_MAPS_API_KEY: 'AIzaSyBqAdWycDiL9gKP4C9TffHigk34OykQ8ag',
+    GOOGLE_MAPS_API_KEY: '',
     // REVENUE_CAT_API_KEY: '',
     // ADMOB_APP_ID: '',
 };
