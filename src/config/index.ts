b1ac488e6994def63bd3e1a567bce2196ed0f3e6
Dhaval Panchani <developer@identiqinfotech.com>
2026-02-05 15:22:02 +0530

    - implement ad setup for production

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,16 +4,16 @@ import { Secrets } from './secrets';
 export const AppConfig = {
     // AdMob Test IDs
     admob: {
-        appIdAndroid: "ca-app-pub-3940256099942544~3347511713",
-        appIdIOS: "ca-app-pub-3940256099942544~1458002511",
-        bannerAndroid: "ca-app-pub-3940256099942544/6300978111",
-        bannerIOS: "ca-app-pub-3940256099942544/2934735716",
-        interstitialAndroid: "ca-app-pub-3940256099942544/1033173712",
-        interstitialIOS: "ca-app-pub-3940256099942544/4411468910",
-        rewardedAndroid: "ca-app-pub-3940256099942544/5224354917",
-        rewardedIOS: "ca-app-pub-3940256099942544/1712485313",
-        nativeAndroid: "ca-app-pub-3940256099942544/2247696110",
-        nativeIOS: "ca-app-pub-3940256099942544/3986624511",
+        appIdAndroid: Secrets.ADMOB.ANDROID_APP_ID,
+        appIdIOS: Secrets.ADMOB.IOS_APP_ID,
+        bannerAndroid: Secrets.ADMOB.ANDROID_BANNER_ID,
+        bannerIOS: Secrets.ADMOB.IOS_BANNER_ID,
+        interstitialAndroid: Secrets.ADMOB.ANDROID_INTERSTITIAL_ID,
+        interstitialIOS: Secrets.ADMOB.IOS_INTERSTITIAL_ID,
+        rewardedAndroid: "ca-app-pub-3940256099942544/5224354917", // Test ID (Not used yet)
+        rewardedIOS: "ca-app-pub-3940256099942544/1712485313", // Test ID (Not used yet)
+        nativeAndroid: "ca-app-pub-3940256099942544/2247696110", // Test ID (Not used yet)
+        nativeIOS: "ca-app-pub-3940256099942544/3986624511", // Test ID (Not used yet)
     },
     GOOGLE_MAPS_API_KEY: Secrets.GOOGLE_MAPS_API_KEY,
     REVENUECAT_API_KEY_IOS: Secrets.REVENUECAT_API_KEY_IOS,
